44639cd153c0cc307c3ad2a218e8eb82405e3a46
chrisbeltr <christianbeltran@uchicago.edu>
2026-01-06 17:09:14 -0600

    changed install script

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@ cp .bashsetup .bashpre .bashprompt ~/
 echo "" >> ~/.bashrc
 echo "# custom prompt" >> ~/.bashrc
 echo "source ~/.bashsetup" >> ~/.bashrc
-source ~/.bashrc
+echo "if there were no errors on your terminal, you can safely delete this folder and run \"source ~/.bashrc\" to use your new prompt!"
+echo "make sure you turn off any other prompts you have (eg. oh-my-posh)"
